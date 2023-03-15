469e887f0a41dce8d9fba4b1efddefcf986e3106
alex <alejandro_mr7@outlook.com>
2023-03-15 16:06:49 -0500

    Terminando el metodo loggin

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,5 +1,7 @@
-const { json } = require("sequelize");
 const Usuario = require("../models/Usuario")
+const bcrypt = require('bcrypt');
+const generarId = require("../helpers/generarId");
+const generarJWT = require("../helpers/generarJWT");
 
 const registrar = async (req, res) => {
     const { email } = req.body;
@@ -19,4 +21,40 @@ const registrar = async (req, res) => {
     }
 }
 
-module.exports = { registrar }
+
+const autenticar = async (req, res) => {
+    const { email, password } = req.body;
+
+    // Comprobar si el usuario existe //
+    const usuario = await Usuario.findOne({ where: { email } });
+
+    if (!usuario) {
+        const error = new Error('El usuario no existe!');
+        return res.status(404).json({ msg: error.message, error: true });
+    }
+
+    // Comprobar si el usuario esta confirmado //
+    if (!usuario.confirmar) {
+        const error = new Error('Aún no has confirmado tú cuenta!');
+        return res.status(401).json({ msg: error.message, error: true });
+    }
+
+    // Comprobar su password //
+    const compararPassword = await bcrypt.compare(password, usuario.password);
+    console.log(compararPassword);
+
+    if (!compararPassword) {
+        const error = new Error('Contraseña incorrecta!');
+        return res.status(403).json({ msg: error.message, error: true });
+    }
+
+    res.json({
+        id: usuario.id,
+        nombre: usuario.nombre,
+        email: usuario.email,
+        token: generarJWT(usuario.id)
+    });
+
+}
+
+module.exports = { registrar, autenticar }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const router = Router();
-const { registrar } = require('../controllers/usuarioControllers');
+const { registrar, autenticar } = require('../controllers/usuarioControllers');
 
 router.post('/', registrar);
+router.post('/login', autenticar);
 
 module.exports = router;
